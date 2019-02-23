72519ed8bb4e5842215c49dd5692c0e659b82f6c
L. Zhang <lyzhang10@gmail.com>
2019-02-24 12:40:16 +1300

    minor changes

--- R/discover_pi.R
+++ R/discover_pi.R
@@ -7,7 +7,7 @@ L <- 0 # L is for the half of the circumference
 A_half <- 0 # A_half is for the half of the area
 
 for(i in 2:length(x))
-{d <- sqrt((x[i] - x[i-1])** 2 + (y[i] - y[i-1])**2 )
+{d <- sqrt((x[i] - x[i-1])** 2 + (y[i] - y[i-1])**2 ) 
  L <- L + d
  s <- (d + 2*r) / 2
  # using Heron's formula
