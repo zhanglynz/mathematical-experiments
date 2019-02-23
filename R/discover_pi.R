b565561fd378dd8f5a72db652651138a03b09e37
L. Zhang <lyzhang10@gmail.com>
2019-02-23 22:51:00 +1300

    write about "discover pi"

---
+++ R/discover_pi.R
@@ -0,0 +1,18 @@
+NO_of_points <- 10000 
+r <- 10 # radius of circle
+x <- seq(-r, r, length.out = NO_of_points) # x-coordinates
+y <- sqrt(r**2 - x**2) # y-coordinates
+
+L <- 0 # L is for the half of the circumference
+A_half <- 0 # A_half is for the half of the area
+
+for(i in 2:length(x))
+{d <- sqrt((x[i] - x[i-1])** 2 + (y[i] - y[i-1])**2 )
+ L <- L + d
+ s <- (d + 2*r) / 2
+ # using Heron's formula
+ A_half <- A_half + sqrt(s * (s - r) * (s - r) * (s - d)) 
+}
+
+(L/r)
+(2*A_half/r**2)
